0746d9e5747597fb2504c951b8caddee466fda36
Nilanjan Deb <nilanjan172nsvian@gmail.com>
2020-08-02 12:57:03 +0530

    upload working

--- src/redux/actions/files.js
+++ src/redux/actions/files.js
@@ -1,13 +1,22 @@
 import { FILE_UPLOAD, FILE_UPLOAD_LOADING } from './types';
-
+import crypto from 'crypto';
+import path from 'path';
 import firebase from '../../firebase/firebaseApp';
 
 const uploadImageToStorage = ev => async dispatch => {
 	const storage = firebase.storage();
+	const extName = path.extname(ev.file.name);
+	const fileName = `${crypto
+		.randomBytes(15)
+		.toString('hex')
+		.toUpperCase()}${extName}`;
 	try {
 		dispatch({
 			type: FILE_UPLOAD_LOADING,
 		});
+		const storageRef = storage.ref(`uploads/${fileName}`);
+		const snapshot = await storageRef.put(ev.file);
+		console.log(snapshot);
 	} catch (e) {
 		console.log(e);
 	}
